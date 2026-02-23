d15f8912cc49b637d1e1281db5c7add329d65b4b
Varunyadavgithub <yadav.varun056038@gmail.com>
2026-02-23 14:34:07 +0530

    update the connection method db.js file

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -2,55 +2,64 @@ import sql from "mssql";
 import dotenv from "dotenv";
 dotenv.config();
 
-// Config for Server 1
+// Config for Database 1
 export const dbConfig1 = {
   user: process.env.DB_USER1,
   password: process.env.DB_PASSWORD1,
   server: process.env.DB_SERVER1,
   database: process.env.DB_NAME1,
   options: {
-    encrypt: true,
+    encrypt: false,
     trustServerCertificate: true,
   },
 };
 
-// Config for Server 2
+// Config for Database 2
 export const dbConfig2 = {
   user: process.env.DB_USER2,
   password: process.env.DB_PASSWORD2,
   server: process.env.DB_SERVER2,
   database: process.env.DB_NAME2,
   options: {
-    encrypt: true,
+    encrypt: false,
     trustServerCertificate: true,
   },
 };
 
-// Config for Server 3
+// Config for Database 3
 export const dbConfig3 = {
   user: process.env.DB_USER3,
   password: process.env.DB_PASSWORD3,
   server: process.env.DB_SERVER3,
   database: process.env.DB_NAME3,
   options: {
-    encrypt: true,
+    encrypt: false,
     trustServerCertificate: true,
   },
 };
 
 // Function to connect to any given config
+const pools = new Map();
+
 export const connectToDB = async (dbConfig) => {
+  const key = `${dbConfig.server}_${dbConfig.database}`;
+
+  if (pools.has(key)) {
+    console.log(`Reusing pool: ${dbConfig.database}`);
+    return pools.get(key);
+  }
+
   try {
-    const pool = await sql.connect(dbConfig);
-    console.log(`Connected to SQL Server: ${dbConfig.server}`);
+    const pool = new sql.ConnectionPool(dbConfig);
+    await pool.connect();
+
+    pools.set(key, pool);
+
+    console.log(`Connected to ${dbConfig.database} ---> ${dbConfig.server}`);
+
     return pool;
   } catch (err) {
-    console.error(
-      `Database connection failed for server ${dbConfig.server}:`,
-      err
-    );
+    console.error(`DB connection failed: ${dbConfig.database}`, err);
     throw err;
   }
 };
-
-export default sql;
